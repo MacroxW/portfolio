2d330c38b3d7d5b2100937b15e4f1da77622865b
MacroxW <wendymarcos2@gmail.com>
2024-03-20 14:46:05 -0300

    init portfolio

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+const config = {
+    email: "wendymarcos2@gmail.com",
+    github: "https://github.com/MacroxW",
+    linkedin: "https://www.linkedin.com/in/marcoswendy/"
+}
+
+export default config;
