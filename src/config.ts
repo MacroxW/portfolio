7303e0d65ee3704ed0065b964440a41b8ef3bc85
MacroxW <wendymarcos2@gmail.com>
2026-07-17 11:34:27 -0300

    feat: enhance theme toggle accessibility and add new themes; create TransactionScroll component; update layout and index page with improved structure and content

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 const config = {
-    email: "wendymarcos2@gmail.com",
-    github: "https://github.com/MacroxW",
-    linkedin: "https://www.linkedin.com/in/marcoswendy/"
+  email: "wendymarcos2@gmail.com",
+  github: "https://github.com/MacroxW",
+  linkedin: "https://www.linkedin.com/in/marcoswendy/",
+  location: "Buenos Aires, Argentina",
 }
 
-export default config;
+export default config
